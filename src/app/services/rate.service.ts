b886df398c6c526013ad1ceb8a86eeca5cf4ae0e
remi.pro <remi.arnaud.pro@outlook.fr>
2024-04-13 16:33:48 +0200

    rate service

--- src/app/services/rate.service.ts
+++ src/app/services/rate.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RateService {
+  private baseUrl = 'https://api.coincap.io/v2/rates';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRate(currencyId: string): Observable<number> {
+    return this.http.get<{ data: any }>(`${this.baseUrl}/${currencyId}`).pipe(
+      map(response => parseFloat(response.data.rateUsd))
+    );
+  }
 }
